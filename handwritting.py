8f43966127d9e81fa27ad7464e99bcea06883562
electrotechsolution <electrotech6306@gmail.com>
2022-07-30 13:01:12 +0530

    'Handwrittings'

---
+++ handwritting.py
@@ -0,0 +1,9 @@
+# Author: Abdul Raza
+# Version: 0.1
+# Date: 22th November 2021
+import pywhatkit as pw
+txt ="""What is Lorem Ipsum?."""
+ 
+# pw.text_to_handwriting(txt,"img1.png",[0,0,138])
+pw.text_to_handwriting(txt)
+print("END")
